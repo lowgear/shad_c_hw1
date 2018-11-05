17ac6eb5a7fed50031ae4b5240cb01239b6c7ba8
lowgear <lowgear1000@gmail.com>
2018-11-06 02:20:23 +0500

    some tests

---
+++ test_framework.h
@@ -0,0 +1,53 @@
+#define ASSERT_TRUE(v) \
+if (!(v)) throw 1;
+
+#define ASSERT_FALSE(v) \
+if ((v)) throw 1;
+
+#define ASSERT_EQUAL(a, b) \
+if ((a) != (b)) throw 1;
+
+#define ASSERT_NOT_EQUAL(a, b) \
+if ((a) == (b)) throw 1;
+
+void Dummy() {}
+
+void (*TestsEntryPoint)() = Dummy;
+
+#define TEST(name) \
+void name##_test(); \
+\
+struct name##_class { \
+    static void (*next)(); \
+\
+    static void Run() { \
+        name##_test(); \
+        next(); \
+    } \
+\
+    name##_class() { \
+        next = TestsEntryPoint; \
+        TestsEntryPoint = name##_class::Run; \
+    } \
+}; \
+\
+void (*name##_class::next)() = nullptr; \
+name##_class static_instance_##name; \
+\
+void name##_check(); \
+void name##_test() { \
+    printf("%s testing: ", #name); \
+    try { \
+        name##_check(); \
+    } \
+    catch (...) { \
+        printf("FAIL\n"); \
+        return; \
+    } \
+    printf("OK\n"); \
+} \
+void name##_check()
+
+int main() {
+    TestsEntryPoint();
+}
--- tests.cc
+++ tests.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cstring>
+#include "test_framework.h"
 
 extern "C" {
 #include "number_operations.h"
@@ -7,73 +8,6 @@ extern "C" {
 #include "str_ops.h"
 }
 
-
-//#define TEST(fixture, test) \
-//void fixture##_##test##_check(); \
-//void fixture##_##test() { \
-//    printf("%s_%s testing: ", #fixture, #test); \
-//    try { \
-//        fixture##_##test##_check(); \
-//    } \
-//    catch (...) { \
-//        printf("FAIL\n"); \
-//        return; \
-//    } \
-//    printf("OK\n"); \
-//} \
-//void fixture##_##test##_check()
-//
-//#define RUN_TEST(fixture, test) fixture##_##test();
-
-#define ASSERT_TRUE(v) \
-if (!(v)) throw 1;
-
-#define ASSERT_FALSE(v) \
-if ((v)) throw 1;
-
-#define ASSERT_EQUAL(a, b) \
-if ((a) != (b)) throw 1;
-
-#define ASSERT_NOT_EQUAL(a, b) \
-if ((a) == (b)) throw 1;
-
-void Dummy() {}
-
-void (*TestsEntryPoint)() = Dummy;
-
-#define TEST(name) \
-void name##_test(); \
-\
-struct name##_class { \
-    static void (*next)(); \
-\
-    static void Run() { \
-        name##_test(); \
-        next(); \
-    } \
-\
-    name##_class() { \
-        next = TestsEntryPoint; \
-        TestsEntryPoint = name##_class::Run; \
-    } \
-}; \
-\
-void (*name##_class::next)() = nullptr; \
-name##_class static_instance_##name; \
-\
-void name##_check(); \
-void name##_test() { \
-    printf("%s testing: ", #name); \
-    try { \
-        name##_check(); \
-    } \
-    catch (...) { \
-        printf("FAIL\n"); \
-        return; \
-    } \
-    printf("OK\n"); \
-} \
-void name##_check()
 
 TEST(ReverseStr_ShouldReverseOdd) {
     char original[] = "abcde";
@@ -89,7 +23,9 @@ TEST(ReverseStr_ShouldReverseEven) {
     ASSERT_TRUE(strcmp(original, "dcba") == 0)
 }
 
-int main() {
-    TestsEntryPoint();
-    return 0;
+TEST(ReverseStr_ShouldWorkWithEmptyString) {
+    char original[] = "";
+    ReverseStr(original);
+
+    ASSERT_TRUE(strcmp(original, "") == 0)
 }
