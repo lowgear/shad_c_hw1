e583d3aea5c9ca34c29df601bedee8be76dbfded
lowgear <lowgear1000@gmail.com>
2018-12-19 15:03:26 +0500

    fix malloc.h

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <inttypes.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 #include "number_operations.h"
 
--- number_operations.c
+++ number_operations.c
@@ -1,4 +1,4 @@
-#include <malloc.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <inttypes.h>
